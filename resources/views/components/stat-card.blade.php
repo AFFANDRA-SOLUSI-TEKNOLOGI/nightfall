0418fc184bd1b4bc7c415b2132cf6741498ce1fd
JastinXyz <jastinlinggar@gmail.com>
2024-02-08 11:07:33 +0700

    change background color on icon slot in stat card

--- resources/views/components/stat-card.blade.php
+++ resources/views/components/stat-card.blade.php
@@ -1,6 +1,6 @@
-<div class="p-4 lg:p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-xl rounded-lg">
+<div class="p-4 lg:p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-lg rounded-lg">
     <div class="flex items-center gap-2">
-        <div class="h-10 w-10 bg-gray-700 rounded-full text-white flex justify-center items-center">
+        <div class="h-10 w-10 bg-base-content/10 dark:bg-gray-700 text-gray-900 dark:text-white rounded-full flex justify-center items-center">
             {{ $icon }}
         </div>
     </div>
