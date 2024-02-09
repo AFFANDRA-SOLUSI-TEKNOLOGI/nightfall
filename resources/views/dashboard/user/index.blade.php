75c2dd5f0bf09bbe17f27dd4e2ead76dae1ef8d5
JastinXyz <jastinlinggar@gmail.com>
2024-02-09 13:51:39 +0700

    feat: print example

--- resources/views/dashboard/user/index.blade.php
+++ resources/views/dashboard/user/index.blade.php
@@ -4,7 +4,10 @@
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 {{ __('User') }}
             </h2>
-            <x-button variant="primary" href="{{ route('user.create') }}">Add</x-button>
+            <div class="flex gap-2">
+                <x-button variant="primary" href="{{ route('user.print') }}" target="_blank" hardRefresh>Print</x-button>
+                <x-button variant="primary" href="{{ route('user.create') }}">Add</x-button>
+            </div>
         </div>
     </x-slot>
 
